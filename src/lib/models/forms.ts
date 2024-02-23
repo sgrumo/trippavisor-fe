883cd385a8ef33f823b01dde9993592b6c1d8ad0
Samuele Medici <samuele.medici@quinck.io>
2024-02-23 10:39:19 +0100

    feat: :rewind: removed qwik-lottie animation library

--- src/lib/models/forms.ts
+++ src/lib/models/forms.ts
@@ -3,6 +3,7 @@ import {
   boolean,
   date,
   maxValue,
+  minLength,
   minValue,
   number,
   object,
@@ -12,10 +13,20 @@ import {
 } from "valibot";
 import { MAX_RADIUS, MIN_RADIUS } from "../constants/generics";
 
+const QUERY_MIN_LENGTH = 3;
+
+const LATITUDE_MIN = -90;
+const LATITUDE_MAX = 90;
+
+const LONGITUDE_MIN = -180;
+const LONGITUDE_MAX = 180;
+
 export const SearchSchema = object({
-  query: optional(string([])),
-  latitude: optional(number()),
-  longitude: optional(number()),
+  query: optional(string([minLength(QUERY_MIN_LENGTH)])),
+  latitude: optional(number([minValue(LATITUDE_MIN), maxValue(LATITUDE_MAX)])),
+  longitude: optional(
+    number([minValue(LONGITUDE_MIN), maxValue(LONGITUDE_MAX)]),
+  ),
   range: optional(number([minValue(MIN_RADIUS), maxValue(MAX_RADIUS)])),
   date: optional(date([])),
   tags: object({ array: array(string()), boolean: boolean() }),
